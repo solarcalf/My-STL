c130018cfbffaab89b4620e96c360dfe623bcab4
Egor Tanin <sealynxxxx@gmail.com>
2023-05-17 21:08:12 +0300

    Stack implementation

---
+++ Stack/Stack/Stack.h
@@ -0,0 +1,57 @@
+#ifndef _Stack
+#define _Stack
+
+#define ND [[nodiscard]]
+
+#include <deque>
+#include <iostream>
+
+template<typename T, class Container = std::deque<T>> 
+class Stack {
+public:
+	using container_type	= Container;
+	using value_type		= typename Container::value_type;
+	using size_type			= typename Container::size_type;
+	using reference			= typename Container::reference;
+	using const_reference	= typename Container::const_reference;
+
+	Stack() = default;
+	Stack(const Stack<T>& s) : cont(s.cont) {}
+	Stack(Stack<T>&& s) noexcept : cont(std::move(s.cont)) {}
+	explicit Stack(const Container& cont) : cont(cont) {}
+	explicit Stack(Container&& cont) noexcept(std::is_nothrow_move_constructible_v<Container>) : cont(std::move(cont)) {}
+	Stack& operator=(const Stack& rhs) {
+		cont = rhs.cont;
+		return *this;
+	}
+	Stack& operator=(Stack&& rhs) noexcept {
+		cont = std::move(rhs);
+		return *this;
+	}
+
+	reference top() noexcept(noexcept(cont.back())) { return cont.back(); }
+	const_reference top() const noexcept(noexcept(cont.back())) { return cont.back(); }
+
+	ND bool empty() const noexcept(noexcept(cont.empty())) { return cont.empty(); }
+	ND size_type size() const noexcept(noexcept(cont.size())) { return cont.size(); }
+
+	void push(const value_type& val) { cont.push_back(val); }
+	void push(value_type&& val) { cont.push_back(std::move(val)); }
+	template< class... Args >
+	void emplace(Args&&... args) { cont.emplace_back(std::forward<Args>(args)...); }
+	void pop() noexcept(noexcept(cont.pop_back())) { cont.pop_back(); }
+	void swap(Stack<T>& rhs) noexcept(std::is_nothrow_swappable_v<Container>) { std::swap(cont, rhs.cont); }
+
+	ND const Container& _Get_container() const noexcept { return cont; }
+
+protected:
+	Container cont;
+};
+
+template <typename T, class Container>
+void swap(Stack<T, Container>& lhs, Stack<T, Container>& rhs) noexcept(std::is_nothrow_swappable_v<Container>) {
+	std::swap(lhs._Get_container(), rhs._Get_container());
+}
+
+
+#endif // _Stack
